15ac9af92b675b71c6fa8af4dc6ab8b77943c0c6
Sean Prouty <prouty_411@hotmail.com>
2018-07-24 10:30:16 -0700

    added movieInforApi

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(initializeApp);
+
+function initializeApp(){
+    getMovieInfoApi();
+}
+
+
+
+function getMovieInfoApi(){
+    console.log('inside fnct');
+    var movieInfo = {
+          dataType:'json',
+          url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=90ec7552787d25df49e8eac53f951398&language=en-US&page=1',
+          data:{api_key:'90ec7552787d25df49e8eac53f951398'},
+          method:"GET",
+          success:function(response){
+                console.log(response);
+            
+          }
+          }
+    $.ajax(movieInfo);
+    }
