6bac03876e7d83d03534484c6b97901575cb3055
Sean Prouty <prouty_411@hotmail.com>
2018-07-25 15:05:05 -0700

    merged complete

--- restructured.js
+++ restructured.js
@@ -2,9 +2,12 @@ $(document).ready(initializeApp);
 var map;
 var geocoder;
 var globalMarkers = [];
+var movieArray = [];
+var movieObj = {}
 
 function initializeApp() {
-    // myMap();
+    getMovieInfoApi();
+    $('#trailerModal').modal('hide');
     displayFoodInArea();
     getCustomPlaces();
     addHandlers();
@@ -12,6 +15,78 @@ function initializeApp() {
 
 function addHandlers() {
     $(".topHalf button").on('click', determineStartCoords);
+}
+
+function movieTrailer(title) {
+    var ajaxOptions = {
+        url: 'https://s-apis.learningfuze.com/hackathon/youtube/search.php',
+        dataType: 'json',
+        method: 'post',
+        data: {
+            'q': title + ' trailer',
+            'maxResults': 1
+
+        },
+        success: function (response) {
+            var videoID = response.video[0].id;
+            $('#trailerModal').modal('show')
+            $('#videoTrailer').attr('src', `https://www.youtube.com/embed/${videoID}?autoplay=1`);
+
+        },
+        error: function () {
+            console.log('error')
+        }
+    }
+    $.ajax(ajaxOptions)
+}
+
+function renderMovieOnDom(movie){
+    var movieDiv = $('<div>');
+    movieDiv.addClass('item');
+    var poster = $('<img>', {
+          src: movie.moviePoster,
+          height: '85%'
+    });
+    var rating = $('<div>').text(movie.movieRating);
+    movieDiv.append(poster, rating);
+    movieDiv.on('click', movieTrailer)
+    $('.carousel-inner').append(movieDiv)
+    $('.item').first().addClass('active');
+}
+
+
+function displayMovieList(response){
+    var movies = response.results;
+    var webAdd = "https://image.tmdb.org/t/p/w185";
+    
+    for(var index = 0; index < 16; index++){
+          var webImage = movies[index].poster_path;
+          var movieTitle = movies[index].title;
+          var moviePoster = webAdd + webImage;
+          var movieRating = movies[index].vote_average + ' /10';
+          movieObj = {
+                movieTitle,
+                moviePoster,
+                movieRating
+          };
+          movieArray.push(movieObj);
+          renderMovieOnDom(movieObj);
+    }
+}
+
+function getMovieInfoApi(){
+    var movieInfo = {
+          dataType:'json',
+          url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=90ec7552787d25df49e8eac53f951398&language=en-US&page=1',
+          data:{api_key:'90ec7552787d25df49e8eac53f951398'},
+          method:"GET",
+          success:function(response){
+              console.log(response);
+            displayMovieList(response);
+          }
+    }
+    $.ajax(movieInfo);
+
 }
 
 function getCustomPlaces(result, status) {
@@ -39,9 +114,10 @@ function placeCallback(results, status) {
             map.setCenter(results[0].geometry.location);
             var marker = new google.maps.Marker({
                 map: map,
-                position: place.geometry.location
+                position: place.geometry.location,
+                animation: google.maps.Animation.DROP
             });
-            console.log(marker)
+            
             globalMarkers.push(marker);
         }
     }
@@ -74,14 +150,20 @@ function displayFoodInArea() {
             api_key: "XSyryzoREYThrY1P0pDAkbK9uJV0j7TVklsKegO9g9aqqqGz87SZPuhQ0Cob0jzZ6G1BCVE9JaycPHyB2OI7hXgTJYs_enS7SKr1G21Jf45cDBYbUAHOFnh-r3FWW3Yx"
         },
         success: function (response) {
-            var userInput = $('#searchbar').val();
-            $('button').on('click', function () {
-                // console.log(userInput)
-            })
             var arrayOfBusinesses = response.businesses;
-            // console.log(arrayOfBusinesses[0].coordinates.latitude, arrayOfBusinesses[0].coordinates.longitude);
-
-
+            console.log(arrayOfBusinesses);
+            for(var i = 0; i < 6; i++){
+                var newDiv = $('<div>');
+                var newh3 = $('<h4>');
+                var newImage = $('<img>');
+                newh3.text(arrayOfBusinesses[i].name);
+                newImage.attr('src', arrayOfBusinesses[i]['image_url']);
+                newDiv.append(newh3, newImage);
+                $('.bottom_area').append(newDiv);
+                var newPtag = $('<h4>').text(`Phone Number: ${arrayOfBusinesses[i].phone.slice(2)}`);
+                var pricePTag = $('<h4>').text(`Price: ${arrayOfBusinesses[i].price}`)
+                newDiv.append(newPtag, pricePTag)
+            }
         },
         error: function () {
             console.log('error');
@@ -105,6 +187,22 @@ function myMap() {
     // google.maps.event.addListener(marker, 'click', function(){
     //     console.log('clicked');
     // });
+}
+
+function clickMovieTheaters(){
+    var yelpTheaters = {
+          "url": "https://yelp.ongandy.com/businesses",
+          "method": "POST",
+          "dataType": "JSON",
+          "data": {
+              term: "theaters",
+              location: "long beach",
+              api_key: "XSyryzoREYThrY1P0pDAkbK9uJV0j7TVklsKegO9g9aqqqGz87SZPuhQ0Cob0jzZ6G1BCVE9JaycPHyB2OI7hXgTJYs_enS7SKr1G21Jf45cDBYbUAHOFnh-r3FWW3Yx"
+          },
+          success: function (response){
+                
+          }
+    }
 }
 
 function initAutocomplete() {
